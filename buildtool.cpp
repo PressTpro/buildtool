b8f28f0d04991598181550f75af32303a8b53acc
PressTpro <95140308+PressTpro@users.noreply.github.com>
2023-06-04 16:51:33 -0500

    Create buildtool.cpp

---
+++ buildtool.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+struct BuildConfig {
+    std::string compiler;
+    std::string inputFile;
+    std::string outputFile;
+    bool saveOutput;
+};
+
+BuildConfig readConfig(const std::string& filename) {
+    BuildConfig config;
+    std::ifstream file(filename);
+    std::string line;
+    while (std::getline(file, line)) {
+        if (!line.empty() && line[0] != '#') { // Ignore empty lines and lines starting with #
+            std::stringstream ss(line);
+            std::string key, value;
+            if (std::getline(ss, key, '=')) {
+                if (std::getline(ss, value)) {
+                    if (key == "compiler") {
+                        config.compiler = value;
+                    } else if (key == "SAVE_OUTPUT") {
+                        config.saveOutput = true;
+                    } else if (key == "inputFile") {
+                        config.inputFile = value;
+                    } else if (key == "outputFile") {
+                        config.outputFile = value;
+                    }
+                    // Add more configuration options parsing here as needed
+                }
+            }
+        }
+    }
+    return config;
+}
+
+int main() {
+    BuildConfig config = readConfig("build.inf");
+
+    std::string command = config.compiler;
+    if (config.saveOutput && !config.outputFile.empty()) {
+        command += " -o " + config.outputFile;
+    }
+    command += " " + config.inputFile;
+
+    std::cout << "Running command: " << command << std::endl;
+    std::system(command.c_str());
+
+    return 0;
+}
